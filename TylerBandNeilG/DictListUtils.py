e89bcdc292dba7e579077c7d74776fe41a557be5
Tyler Black <blac0403@algonquinlive.com>
2016-11-01 19:59:37 -0400

    Updated exercise template.

--- TylerBandNeilG/DictListUtils.py
+++ TylerBandNeilG/DictListUtils.py
@@ -9,26 +9,33 @@ __testCount = 0
 def main():
     test_getMissingKeys()
     print "\n%i of %i tests passed" % (__testPassed, __testCount)
-    help(__name__)
+    # If you uncomment the following help statement, help for this module will be
+    # output to the interpreter console.  You will notice the variables prefixed
+    # with double underscores are not displayed.  Prefixing variables and
+    # functions with double underscores "hides" them from global scope.
+    #
+    #help(__name__)
 
 def getMissingKeys(dictRef, dictToCompare):
-    missing = []
-    for keys in dictRef:
-        if dictToCompare.has_key == False:
-            missing.extend()
+    missing =[]
+    for item in dictRef.keys():
+        if dictToCompare.has_key(item) is False:
+            missing.append(item)
     return missing
 
-def func2(params):
-    pass
+def getMissingKeysWithCount(dictRef, dictToCompare):
+
+    return len(missing).append(getMissingKeys(dictRef, dictToCompare))
+
 
 def test_getMissingKeys():
     # Test case 1
     expect = '[1,3]'
-    actual = getMissingKeys({1:1, 2:2, 3:3}, {2:2})
+    actual = getMissingKeys({1:1, 2:2, 3:3}, {2:4})
     funcName = inspect.stack()[0][3][5:]
     printTestResult(funcName, expect, actual)
 
-    # Test case 2 ...
+    print getMissingKeysWithCount({1:1, 2:2, 3:3}, {2:4})
 
 def printTestResult(funcName, expect, actual):
     global __testPassed
