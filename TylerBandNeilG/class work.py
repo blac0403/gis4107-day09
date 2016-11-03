2155c657e08246b243b1479980d83c8ed31f69ee
Neil Gilchrist <neil.g.gilchrist@gmail.com>
2016-11-03 12:48:49 -0400

    changes

--- TylerBandNeilG/class work.py
+++ TylerBandNeilG/class work.py
@@ -52,7 +52,6 @@ def flattenList(inList):
             for newInstance in instance:
                 flatList.append(newInstance)
         if type(instance) == list:
-            list(instance)
             for newInstance in instance:
                 flatList.append(newInstance)
 
